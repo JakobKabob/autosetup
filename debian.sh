07de95c0bd278ffab69487513ced7760da764569
KG55 <jochem@kg55.na>
2022-01-08 22:38:24 +0000

    bugfix 2

--- debian.sh
+++ debian.sh
@@ -7,7 +7,10 @@ fi
 sudo apt update && sudo apt upgrade -y
 sudo apt install tmux neovim -y
 cat tmux.conf.basic > ~/.tmux.conf
-[[ -d ~/.config ]] || mkdir ~/.config ~/.config/nvim
-[[ -d ~/.config/nvim ]] || mkdir ~/.config/nvim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+[[ -d $HOME/.config ]] || mkdir $HOME/.config $HOME/.config/nvim
+[[ -d $HOME/.config/nvim ]] || mkdir $HOME/.config/nvim
 cat init.vim.basic > ~/.config/nvim/init.vim
 
