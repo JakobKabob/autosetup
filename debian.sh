758a293f4807f5f69033cea9d6c3451af5493fb6
KG55 <jochem@kg55.na>
2022-01-08 22:34:10 +0000

    bugfix 1

--- debian.sh
+++ debian.sh
@@ -8,6 +8,6 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install tmux neovim -y
 cat tmux.conf.basic > ~/.tmux.conf
 [[ -d ~/.config ]] || mkdir ~/.config ~/.config/nvim
-[[ -d ~/.config/nvim ]] || mkdir ~/config/nvim
+[[ -d ~/.config/nvim ]] || mkdir ~/.config/nvim
 cat init.vim.basic > ~/.config/nvim/init.vim
 
