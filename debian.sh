1202b29b85ad90a1702c43d0c81b15892ec5b560
KG55 <jochem@kg55.na>
2022-01-08 22:25:36 +0000

    proper if

---
+++ debian.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! sudo true; then
+    echo 'sudo?'
+    exit 1
+fi
+sudo apt update && sudo apt upgrade -y
+sudo apt install tmux neovim -y
+cat tmux.conf.basic > ~/.tmux.conf
+[[ -d ~/.config ]] || mkdir ~/.config ~/.config/nvim
+[[ -d ~/.config/nvim ]] || mkdir ~/config/nvim
+cat init.vim.basic > ~/.config/nvim/init.vim
+
